10a36010d1839b44c8341c423bd0175ca7d8a054
Felipe Hoch <felipe.hoch@sirros.net>
2022-08-25 16:36:41 -0300

    Adds NgRx files.

--- apps/tour/src/app/app.module.ts
+++ apps/tour/src/app/app.module.ts
@@ -5,27 +5,47 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+
     BrowserAnimationsModule,
     RouterModule.forRoot([
       {
         path: 'admin',
-        loadChildren: () => import('@packt/admin').then(m => m.AdminModule)
+        loadChildren: () => import('@packt/admin').then((m) => m.AdminModule),
       },
       {
         path: 'tour',
-        loadChildren: () => import('@packt/visitor').then(m => m.VisitorModule)
+        loadChildren: () =>
+          import('@packt/visitor').then((m) => m.VisitorModule),
       },
       {
-        path: "",
+        path: '',
         pathMatch: 'full',
-        redirectTo: 'tour'
+        redirectTo: 'tour',
+      },
+    ]),
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
       }
-    ])
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
